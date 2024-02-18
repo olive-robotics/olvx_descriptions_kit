663a442feefcb9394b93940d8c1df7350ce5fc85
Edwin <edwin.babaians@gmail.com>
2024-02-18 22:36:23 +0100

    Update.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Olive Kit Description
+
+This package contains ROS-related information about individual olive kit such as olive OWL, olive ANT.
+
+It has .urdf definitions, meshes, collision info etc.
+
+## Usage
+
+```
+git clone https://gitlab.com/oliverobotics/hardware/descriptions/olv_kit_descriptions.git
+
+colcon build
+
+source install/setup.bash
+
+ros2 launch olv_kit_descriptions visualize_olive_imu.launch.py
+
+```
---
+++ launch/visualize_olive_owl.launch.py
@@ -0,0 +1,36 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    
+
+    xacro_file = os.path.join(get_package_share_directory('olv_kit_descriptions'), 'urdf',
+                                     'olv-cam01.urdf')
+    robot_description = Command(
+        [FindExecutable(name='xacro'), ' ', xacro_file])
+
+    rviz_file = os.path.join(get_package_share_directory('olv_module_descriptions'), 'rviz',
+                             'visualize_olive.rviz')
+
+    return LaunchDescription([
+        
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}],
+        ),
+        Node(package='rviz2',
+             executable='rviz2',
+             name='rviz2',
+             arguments=['--display-config', rviz_file])
+        
+        
+    ])
